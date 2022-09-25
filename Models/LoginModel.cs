e815469551768f85219fcd2a841b7a3fd1cc9088
Nantawat Coombs <nantawat.coombs@quantiumfund.com>
2022-09-25 22:16:06 +0700

    Implemented email & pass authentication w/ Firebase

---
+++ Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoombsBank.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+    }
+}
