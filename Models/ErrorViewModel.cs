eee15a2b7f3f9f79edc690e7efe6fc2a046cb5de
Nantawat Coombs <nantawat.coombs@quantiumfund.com>
2022-09-25 21:35:07 +0700

    Init project
    Init web app MVC with .net 6

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoombsBank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
